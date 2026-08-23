8f56d42c78dc39cd2b8372383acb02b50bf46e31
gjd1281 <gjd1281@hotmail.com>
2026-08-23 13:49:09 +1000

    Update cache version from v1 to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shift-engine-v1';
+const CACHE='shift-engine-v2';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e=>{
